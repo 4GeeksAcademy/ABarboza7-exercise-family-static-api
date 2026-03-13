905cca9a7fc3f91488c5f8d654f9b810d2913a86
ABarboza7 <164107449+ABarboza7@users.noreply.github.com>
2026-03-13 21:58:50 +0000

    test compleete

--- src/app.py
+++ src/app.py
@@ -30,16 +30,32 @@ def sitemap():
 
 
 @app.route('/members', methods=['GET'])
-def handle_hello():
-    # This is how you can use the Family datastructure by calling its methods
+def get_all_members():
     members = jackson_family.get_all_members()
-    response_body = {"hello": "world",
-                     "family": members}
-    return jsonify(response_body), 200
+    return jsonify(members), 200
+
+@app.route('/members', methods=['POST'])
+def add_member():
+    member_data = request.get_json()
+    new_member = jackson_family.add_member(member_data)
+    return jsonify(new_member), 200
+
+@app.route('/members/<int:member_id>', methods=['GET'])
+def get_single_member(member_id):
+    member = jackson_family.get_member(member_id)
+    if member:
+        return jsonify(member), 200
+    return jsonify({"msg": "No encontrado"}), 404
+
+@app.route('/members/<int:member_id>', methods=['DELETE'])
+def delete_member(member_id):
+    deleted = jackson_family.delete_member(member_id)
+    if deleted:
+
+        return jsonify({"done": True}), 200
+    return jsonify({"done": False}), 404
 
 
-
-# This only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=True)
